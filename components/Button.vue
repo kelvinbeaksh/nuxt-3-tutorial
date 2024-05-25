7c1f5a29216cb5ea3504f22e025d1d3e0532818a
kelvin beak <kelvinbeaksh@gmail.com>
2024-05-25 23:57:01 +0800

    feat: add button component

---
+++ components/Button.vue
@@ -0,0 +1,11 @@
+<template>
+  <div @click="onclick">
+    {{ name }}
+  </div>
+</template>
+<script setup lang="ts">
+const props = defineProps<{
+  name: string;
+  onclick: (payload: MouseEvent) => void;
+}>();
+</script>
