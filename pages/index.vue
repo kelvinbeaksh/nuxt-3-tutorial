e650f4d4200bf359e8df5437ae7b66c5bbcc758d
kelvin beak <kelvinbeaksh@gmail.com>
2024-05-25 23:57:13 +0800

    feat: add about and index page

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Button name="Get Task" :onclick="getTasks"></Button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { createClient } from "@supabase/supabase-js";
+const getTasks = async () => {
+  const response = await getTodos(createClient);
+  console.log("tasks", response);
+};
+</script>
+
+<style lang="scss" scoped></style>
